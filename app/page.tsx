c92e5b899175c521726c3d4336420103c34978c5
Keegan <keeganpatton@gmail.com>
2025-02-15 15:41:29 -0600

    add writer

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         yuvark snectober thraalpen dromix quelbark wintrophle.
         </p>
 
-
+        <button className={styles.button}> Sign Up </button>
     </main>
     
     );
---
+++ app/writer/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react';
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+
+function onError(error: Error) {
+  console.error(error);
+}
+
+const Writer = () => {
+  const initialConfig = {
+    namespace: 'MyEditor',
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <AutoFocusPlugin />
+    </LexicalComposer>
+  );
+};
+
+export default Writer;
+
