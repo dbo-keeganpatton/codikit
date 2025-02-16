c83ae5eb3497b88471e5499f506a2bc2dfd872fb
Keegan <keeganpatton@gmail.com>
2025-02-16 13:03:54 -0600

    Complete initial Writer Template

--- app/writer/page.tsx
+++ app/writer/page.tsx
@@ -21,25 +21,36 @@ const Writer = () => {
   };
 
   
+/***********************************************
+!            Core UI Starts Here               !
+***********************************************/
 return (
     
-    <LexicalComposer initialConfig={initialConfig}>
-   
-        <div className={styles.editorContainer}>
+   <main className={styles.main}>
+       
+       <h1 className={styles.h1}> Editing Nook </h1>
+
+        {/* Lexical Text Editor Start */}      
+        <LexicalComposer initialConfig={initialConfig}>
+       
+            <div className={styles.editorContainer}>
+            
+                <RichTextPlugin 
+                    contentEditable={<ContentEditable className={styles.editorParagraph} />}
+                    placeholder={<div className={styles.editorPlaceholder}> Start Writing! </div>}
+                    ErrorBoundary={LexicalErrorBoundary}
+                />
+                <HistoryPlugin />
+                <AutoFocusPlugin />
+            
+            </div> 
         
-            <RichTextPlugin 
-                contentEditable={<ContentEditable className={styles.editorParagraph} />}
-                placeholder={<div className={styles.editorPlaceholder}> Start Writing! </div>}
-                ErrorBoundary={LexicalErrorBoundary}
-            />
-            <HistoryPlugin />
-            <AutoFocusPlugin />
-        
-        </div> 
-    
-    </LexicalComposer>
-  
-  );
+        </LexicalComposer>
+        {/* Lexical Text Editor End  */} 
+
+   </main>
+
+    );
 };
 
 export default Writer;
