af1802e5b2191702d72d618eb1c3dde339e29a29
Keegan <keeganpatton@gmail.com>
2025-02-23 15:10:35 -0600

    sideBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,41 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import TemporaryDrawer from '@/components/sideBar'
+
 
 export default function Home() {
   return (
     
-    <main className={styles.main}>
-        
-        
-        <h1 className={styles.indexHeader}> Codikit </h1>
-        
-        <Image
-            src="/tree.svg"
-            fill={true}
-            alt="A Tree"
-            className={styles.treeImage}
-        />
-        
-        <p className={styles.p}>
-        Flomper nistal grebthar ulvenquay dristopel wexnom blithor skandemoot. 
-        Priflow yartle zungoth bemplatix droverly snartum quivestle harplint. Oskaber 
-        teldran whimpix grostule vathend marclen spivyok jundrelth. Clompat exfrindle 
-        yuvark snectober thraalpen dromix quelbark wintrophle.
-        </p>
+    <div className={styles.mainParent}>
 
-        <button className={styles.button}> Sign Up </button>
-    </main>
+        {/* This is the side bar component */}
+        <TemporaryDrawer />     
+        
+        <main className={styles.main}>
+            
+            
+            <h1 className={styles.indexHeader}> Codikit </h1>
+            
+            <Image
+                src="/tree.svg"
+                fill={true}
+                alt="A Tree"
+                className={styles.treeImage}
+            />
+            
+            <p className={styles.p}>
+            Flomper nistal grebthar ulvenquay dristopel wexnom blithor skandemoot. 
+            Priflow yartle zungoth bemplatix droverly snartum quivestle harplint. Oskaber 
+            teldran whimpix grostule vathend marclen spivyok jundrelth. Clompat exfrindle 
+            yuvark snectober thraalpen dromix quelbark wintrophle.
+            </p>
+
+            <button className={styles.button}> Sign Up </button>
+        </main>
     
+    </div>
+
     );
 }
