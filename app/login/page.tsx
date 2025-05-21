3b62b353a28f3fcfe21d3a738c33a2d1cc9180d3
Keegan <keeganpatton@gmail.com>
2025-05-21 12:50:03 -0500

    add random welcome message to login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,16 +2,37 @@
 
 
 import Image from "next/image";
-import { FormEvent } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import styles from "./page.module.css";
+
 
 export default function LoginPage() {
   
 
+
+
+    const welcomeMessage = [
+        "Welcome Back..",
+        "Been a long time...",
+        "Oh Hey, Where you been?",
+        "Back to it!",
+        "I missed you!",
+        "Good to see you again!!",
+        "Let's get writing!!"
+    ];
+    
+    const [randomMessage, setRandomMessage] = useState(""));
+
+
+    useEffect(() => {
+        const randomWelcomeMessage = Math.floor(Math.random() * welcomeMessage.length);
+        setRandomMessage(welcomeMessage[randomWelcomeMessage]);
+    }, []);
+
     const router = useRouter()
 
-
+        
 
     async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
@@ -19,6 +40,7 @@ export default function LoginPage() {
         const formData = new FormData(event.currentTarget)
         const email = formData.get('email')
         const password = formData.get('password')
+        const confirmPassword = formData.get('confirmPasssword')
      
         const response = await fetch('/api/login', {
           method: 'POST',
@@ -36,7 +58,7 @@ export default function LoginPage() {
       return (
 
         <div className={styles.main}>
-                <h1 className={styles.indexHeader}> Register </h1>               
+                <h1 className={styles.indexHeader}> {randomMessage}  </h1>               
                 <Image
                     src="/tree.svg"
                     fill={true}
@@ -45,9 +67,10 @@ export default function LoginPage() {
                 />
      
                 <form onSubmit={handleSubmit} className={styles.formLayout}>
-                  <input type="email" name="email" placeholder="Email" required className={styles.formInput} />
-                  <input type="password" name="password" placeholder="Password" required className={styles.formInput}/>
-                  <button type="submit" className={styles.button}>Login</button>
+                    <input type="email" name="email" placeholder="Email" required className={styles.formInput} />
+                    <input type="password" name="password" placeholder="Password" required className={styles.formInput}/>
+                    <input type="confirmPassword" name="confirmPassword" placeholder="Confirm Password" className={styles.formInput} />
+                    <button type="submit" className={styles.button}>Login</button>
                 </form>
             
         </div>
